926576799c41af8cbabf5e5a04c3241d6f799010
Vitali <ipz221_tsvo@student.ztu.edu.ua>
2024-06-03 20:45:11 +0300

    Finish Task3 Lab2

---
+++ lab-2/Singleton/Singleton/Authenticator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class Authenticator
+{
+    private static Authenticator instance;
+    private static readonly object lockObject = new object();
+
+    private Authenticator() { }
+
+    public static Authenticator GetInstance()
+    {
+        lock (lockObject)
+        {
+            if (instance == null)
+            {
+                instance = new Authenticator();
+            }
+            return instance;
+        }
+    }
+
+    public bool AuthenticateUser(string username, string password)
+    {
+        UserData userData = RetrieveUserData(username);
+        if (userData != null)
+        {
+            if (userData.Password == password)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public UserData RetrieveUserData(string username)
+    {
+        if (username == "admin")
+        {
+            return new UserData { Username = "admin", Password = "admin123" };
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public class UserData
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ lab-2/Singleton/Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Authenticator auth1 = Authenticator.GetInstance();
+            Authenticator auth2 = Authenticator.GetInstance();
+
+            Console.WriteLine($"auth1 == auth2: {auth1 == auth2}");
+        }
+    }
+}
