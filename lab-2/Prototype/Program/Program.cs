8438fa8702574b00d3b42db8f3f4f86fded2c204
Vitali <ipz221_tsvo@student.ztu.edu.ua>
2024-06-03 21:02:15 +0300

    Finish Task4 Lab2

---
+++ lab-2/Prototype/Program/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Virus grandparent = new Virus("Grandparent", 5, 10.5, "Coronavirus");
+        Virus parent1 = new Virus("Parent 1", 3, 8.2, "Influenza");
+        Virus parent2 = new Virus("Parent 2", 4, 9.8, "Ebola");
+
+        Virus child1 = new Virus("Child 1", 1, 3.5, "Rhinovirus");
+        Virus child2 = new Virus("Child 2", 2, 4.0, "HIV");
+
+        grandparent.AddChild(parent1);
+        grandparent.AddChild(parent2);
+        parent1.AddChild(child1);
+        parent1.AddChild(child2);
+
+        Virus clonedGrandparent = (Virus)grandparent.Clone();
+
+        Console.WriteLine("Original Virus:");
+        Console.WriteLine(grandparent);
+
+        Console.WriteLine("\nCloned Virus:");
+        Console.WriteLine(clonedGrandparent);
+
+        Console.WriteLine("\nAre original and cloned viruses equal:");
+        Console.WriteLine(grandparent == clonedGrandparent);
+    }
+}
---
+++ lab-2/Prototype/Program/Virus.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Virus : ICloneable
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public double Weight { get; set; }
+    public string Species { get; set; }
+    public List<Virus> Children { get; set; } = new List<Virus>();
+
+    public Virus(string name, int age, double weight, string species)
+    {
+        Name = name;
+        Age = age;
+        Weight = weight;
+        Species = species;
+    }
+
+    public void AddChild(Virus child)
+    {
+        Children.Add(child);
+    }
+
+    public object Clone()
+    {
+        Virus clonedVirus = new Virus(Name, Age, Weight, Species);
+
+        foreach (var child in Children)
+        {
+            clonedVirus.AddChild((Virus)child.Clone());
+        }
+
+        return clonedVirus;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, Age: {Age}, Weight: {Weight}, Species: {Species}";
+    }
+}
